e05d5003e6d7fdc32f092c1d540700d77c039e0b
rutgerva <fioxxu@gmail.com>
2023-03-22 20:52:10 +0100

    AoC Day 2 - Rock, Paper, Scissors (pt. 1) - refactored

--- src/main/java/com/fioxxu/aoc/aoc2022/AoCDay2.java
+++ src/main/java/com/fioxxu/aoc/aoc2022/AoCDay2.java
@@ -1,6 +1,8 @@
 package com.fioxxu.aoc.aoc2022;
 
 import com.fioxxu.aoc.aoc2022.utils.BufferedReaderHelper;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,90 +12,28 @@ public class AoCDay2 {
     private static final String INPUT_FILE = "aoc_day2_input.txt";
     private static Integer points = 0;
 
-    enum Move {
-        ROCK("A", "X", 1),
-        PAPER("B", "Y", 2),
-        SCISSORS("C", "Z", 3),
-        UNKNOWN("", "", 0);
-        private final String alias1;
-        private final String alias2;
-        private final int basePoints;
-
-        Move(String alias1, String alias2, int basePoints) {
-            this.alias1 = alias1;
-            this.alias2 = alias2;
-            this.basePoints = basePoints;
-        }
-
-        public int getBasePoints() {
-            return this.basePoints;
-        }
-
-        public static Move fromAlias(String alias) {
-            for (var move : Move.values()) {
-                if (move.alias1.equals(alias) || move.alias2.equals(alias))
-                    return move;
-            }
-            return UNKNOWN;
-        }
+    enum Moves {
+        ROCK,
+        PAPER,
+        SCISSORS,
+        UNKNOWN
     }
 
-    enum MatchResult {
-        WIN(6),
-        DRAW(3),
-        LOSE(0),
-        UNKNOWN(-1);
-
-        private final int points;
-
-        MatchResult(int points) {
-            this.points = points;
-        }
-
-        public Integer getResultPoints() {
-            return this.points;
-        }
-
-        public static MatchResult matchResult(Move playerMove, Move opponentMove) {
-            if (playerMove.equals(opponentMove))
-                return DRAW;
-            switch (playerMove) {
-                case PAPER -> {
-                    if (opponentMove == Move.ROCK)
-                        return WIN;
-                    else
-                        return LOSE;
-                }
-                case ROCK -> {
-                    if (opponentMove == Move.PAPER)
-                        return LOSE;
-                    else
-                        return WIN;
-                }
-                case SCISSORS -> {
-                    if (opponentMove == Move.ROCK)
-                        return LOSE;
-                    else
-                        return WIN;
-                }
-                case UNKNOWN -> {
-                    return UNKNOWN;
-                }
-            }
-            return UNKNOWN;
-        }
-    }
+    /* Three possible moves */
+    private static final Move ROCK = new Move(Moves.ROCK, Moves.PAPER, Moves.SCISSORS, 1);
+    private static final Move PAPER = new Move(Moves.PAPER, Moves.SCISSORS, Moves.ROCK, 2);
+    private static final Move SCISSORS = new Move(Moves.SCISSORS, Moves.ROCK, Moves.PAPER, 3);
 
     public static void main(String[] args) {
+
         bufferedReader = BufferedReaderHelper.build(INPUT_FILE);
         try {
             String line = bufferedReader.readLine();
             while (null != line) {
                 String[] playerMoves = line.split("\\s+");
-                Move opponent = Move.fromAlias(playerMoves[0]);
-                Move player = Move.fromAlias(playerMoves[1]);
-                points += player.getBasePoints();
-                points += MatchResult.matchResult(player, opponent).getResultPoints();
+                Move opponent = createMove(playerMoves[0]);
+                Move player = createMove(playerMoves[1]);
+                points += player.play(opponent);
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
@@ -101,5 +41,57 @@ public class AoCDay2 {
         }
 
         System.out.println("You should end up with " + points + " points!");
+    }
+
+    private static Move createMove(String alias) {
+        switch (alias) {
+            case "A", "X" -> {
+                return ROCK;
+            }
+            case "B", "Y" -> {
+                return PAPER;
+            }
+            case "C", "Z" -> {
+                return SCISSORS;
+            }
+            default -> {
+                return new Move(Moves.UNKNOWN, Moves.UNKNOWN, Moves.UNKNOWN, 0);
+            }
+        }
+    }
+
+    @RequiredArgsConstructor
+    @EqualsAndHashCode
+    static class Move {
+        private final Moves alias;
+        private final Moves beatenBy;
+        private final Moves beats;
+        private final Integer basePoints;
+
+        private final Integer winPoints = 6;
+        private final Integer drawPoints = 3;
+        private final Integer losePoints = 0;
+
+        private Boolean beats(Moves opponentMove) {
+            return beats.equals(opponentMove);
+        }
+
+        private Boolean beatenBy(Moves opponentMove) {
+            return beatenBy.equals(opponentMove);
+        }
+
+        private Boolean drawsWith(Moves opponentMove) {
+            return alias.equals(opponentMove);
+        }
+
+        final Integer play(Move opponentMove) {
+            if (Boolean.TRUE.equals(opponentMove.beats(this.alias))) {
+                return losePoints + basePoints;
+            } else if (Boolean.TRUE.equals(opponentMove.beatenBy(this.alias))) {
+                return winPoints + basePoints;
+            } else {
+                return drawPoints + basePoints;
+            }
+        }
     }
 }
