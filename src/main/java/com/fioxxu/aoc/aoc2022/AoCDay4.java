1e31408c67cbe2b99c8a8540b08503e0998e2f7a
rutgerva <fioxxu@gmail.com>
2023-03-23 22:09:56 +0100

    AoC Day 4 - Camp Cleanup (pt. 2)

--- src/main/java/com/fioxxu/aoc/aoc2022/AoCDay4.java
+++ src/main/java/com/fioxxu/aoc/aoc2022/AoCDay4.java
@@ -5,10 +5,14 @@ import com.fioxxu.aoc.aoc2022.utils.BufferedReaderHelper;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 public class AoCDay4 {
 
     private static final String INPUT_FILE = "aoc_day4_input.txt";
     private static Integer fullyContainingPairs = 0;
+    private static Integer overlappingPairs = 0;
 
     public static void main(String... args) {
         try {
@@ -18,6 +22,7 @@ public class AoCDay4 {
             while (null != line) {
                 String[] pairs = line.split(",");
                 fullyContainingPairs += fullyContainsOtherPair(pairs[0], pairs[1]) ? 1 : 0;
+                overlappingPairs += hasOverlap(pairs[0], pairs[1]) ? 1 : 0;
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
@@ -25,11 +30,13 @@ public class AoCDay4 {
         }
 
         System.out.println("The assignments contain " + fullyContainingPairs + " fully containing pairs.");
+        System.out.println("The assignments contain " + overlappingPairs + " overlapping containing pairs.");
     }
 
-    private static Boolean fullyContainsOtherPair(String pair1, String pair2) {
+    private static boolean fullyContainsOtherPair(String pair1, String pair2) {
         String[] sectionsPair1 = pair1.split("-");
         String[] sectionsPair2 = pair2.split("-");
+
         int sectionOnePair1 = Integer.parseInt(sectionsPair1[0]);
         int sectionTwoPair1 = Integer.parseInt(sectionsPair1[1]);
         int sectionOnePair2 = Integer.parseInt(sectionsPair2[0]);
@@ -37,10 +44,26 @@ public class AoCDay4 {
 
         if ((sectionOnePair1 >= sectionOnePair2 && sectionTwoPair1 <= sectionTwoPair2)
                 || (sectionOnePair2 >= sectionOnePair1 && sectionTwoPair2 <= sectionTwoPair1)) {
-            return Boolean.TRUE;
+            return TRUE;
         }
 
-        return Boolean.FALSE;
+        return FALSE;
+    }
+
+    private static boolean hasOverlap(String pair1, String pair2) {
+        String[] sectionsPair1 = pair1.split("-");
+        String[] sectionsPair2 = pair2.split("-");
+
+        int sectionOnePair1 = Integer.parseInt(sectionsPair1[0]);
+        int sectionTwoPair1 = Integer.parseInt(sectionsPair1[1]);
+        int sectionOnePair2 = Integer.parseInt(sectionsPair2[0]);
+        int sectionTwoPair2 = Integer.parseInt(sectionsPair2[1]);
+
+        if (sectionTwoPair1 < sectionOnePair2 || sectionOnePair1 > sectionTwoPair2) {
+            return FALSE;
+        }
+
+        return TRUE;
     }
 
 }
